16e96f41fc8d9b1693925c3168eced4b2ac15517
huanghaijie <1019283569@qq.com>
2016-04-11 11:58:55 +0800

    the first commit

---
+++ app/src/main/java/com/hankkin/baidugoingrefreshlayout/MainActivity.java
@@ -0,0 +1,68 @@
+package com.hankkin.baidugoingrefreshlayout;
+
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements BaiDuRefreshListView.OnBaiduRefreshListener{
+
+    private BaiDuRefreshListView mListView;
+    private List<String> mDatas;
+    private ArrayAdapter<String> mAdapter;
+
+    private final static int REFRESH_COMPLETE = 0;
+
+    private Handler mHandler = new Handler(){
+        public void handleMessage(android.os.Message msg) {
+            switch (msg.what) {
+                case REFRESH_COMPLETE:
+                    mListView.setOnRefreshComplete();
+                    mAdapter.notifyDataSetChanged();
+                    mListView.setSelection(0);
+                    break;
+
+                default:
+                    break;
+            }
+        };
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        mListView = (BaiDuRefreshListView) findViewById(R.id.lv);
+        String[] data = new String[]{"a","b","c","d",
+                "e","f","g","h","i",
+                "j","k","l","m","n","o","p","q","r","s"};
+        mDatas = new ArrayList<String>(Arrays.asList(data));
+        mAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,mDatas);
+        mListView.setAdapter(mAdapter);
+        mListView.setOnBaiduRefreshListener(this);
+    }
+
+    @Override
+    public void onRefresh() {
+        new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(3000);
+                    mDatas.add(0, "new data");
+                    mHandler.sendEmptyMessage(REFRESH_COMPLETE);
+                } catch (InterruptedException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+}
